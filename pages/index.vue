9bf716b200a9ab0401d1b031688ef0a3a5487f0c
zhujinglei <zhujinglei2020@outlook.com>
2022-03-23 16:33:33 +0000

    netlify idnetity widget

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+Vue.extend({})
 
-export default Vue.extend({})
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
